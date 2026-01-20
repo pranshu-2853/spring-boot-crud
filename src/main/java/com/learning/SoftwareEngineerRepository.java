6319a748a73fda8966a83ae2f87e115c69da1f8e
pranshu-2853 <pranshu2853@gmail.com>
2026-01-20 23:39:23 +0530

    enable JpaSpecificationExecutor for dynamic filtering

--- src/main/java/com/learning/SoftwareEngineerRepository.java
+++ src/main/java/com/learning/SoftwareEngineerRepository.java
@@ -1,9 +1,11 @@
 package com.learning;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface SoftwareEngineerRepository extends JpaRepository<SoftwareEngineer,Integer> {
+public interface SoftwareEngineerRepository
+        extends JpaRepository<SoftwareEngineer, Integer>,
+        JpaSpecificationExecutor<SoftwareEngineer> {
 
     boolean existsByNameAndTechStack(String name, String techStack);
-
 }

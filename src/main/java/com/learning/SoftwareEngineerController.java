925b0e4be911a9c0219d979f7ab3002ca48bac7b
pranshu-2853 <pranshu2853@gmail.com>
2026-01-22 16:06:24 +0530

    expose filtering and search via controller

--- src/main/java/com/learning/SoftwareEngineerController.java
+++ src/main/java/com/learning/SoftwareEngineerController.java
@@ -4,6 +4,7 @@ import com.learning.dto.PaginatedResponse;
 import com.learning.mapper.PaginationMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import com.learning.dto.SoftwareEngineerFilter;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -27,6 +28,8 @@ public class SoftwareEngineerController {
 
     @GetMapping
     public ResponseEntity<PaginatedResponse<SoftwareEngineerResponseDto>> getSoftwareEngineers(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String techStack,
             @PageableDefault(
                     page = 0,
                     size = 5,
@@ -35,14 +38,19 @@ public class SoftwareEngineerController {
             )
             Pageable pageable) {
 
+        SoftwareEngineerFilter filter = new SoftwareEngineerFilter();
+        filter.setName(name);
+        filter.setTechStack(techStack);
+
         Page<SoftwareEngineerResponseDto> page =
-                softwareEngineerService.getSoftwareEngineers(pageable);
+                softwareEngineerService.getSoftwareEngineers(filter, pageable);
 
         PaginatedResponse<SoftwareEngineerResponseDto> response =
                 PaginationMapper.toPaginatedResponse(page);
 
         return ResponseEntity.ok(response);
     }
+
 
 
 
